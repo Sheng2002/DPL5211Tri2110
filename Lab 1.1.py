859540041f21b632d9eaa8d5bd4a1f895c3a5be4
Sheng2002 <90165898+Sheng2002@users.noreply.github.com>
2021-09-06 10:53:36 +0800

    Add files via upload

---
+++ Lab 1.1.py
@@ -0,0 +1,10 @@
+# student ID :1201200814 #
+# Student Name : Tan Zhi Sheng #
+
+print("I like Python")
+
+person = input("Please enter your name: ")
+print("Hello",person)
+
+# Lab 1.2 get input two numbers and display the sum of the two numbers
+
